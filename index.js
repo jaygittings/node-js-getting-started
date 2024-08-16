7ff37d70015b1b898bc3a301a8f79f347e2a91b8
jaygittings <jaygittings@yahoo.com>
2024-08-16 17:03:50 -0400

    added db call

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const path = require('path')
 const cool = require('cool-ascii-faces')
 
+const { Pool } = require('pg')
+
+const Pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
 const PORT = process.env.PORT || 5001
 
 function showTimes() {
@@ -20,4 +29,16 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', (req, res) => {
+    try {
+      const client = await Pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error = " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
