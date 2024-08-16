9b92514a077b9fc99aea46b2098344d5220e2adb
jaygittings <jaygittings@yahoo.com>
2024-08-16 16:39:16 -0400

    Adding times event var

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const cool = require('cool-ascii-faces')
 
 const PORT = process.env.PORT || 5001
 
+function showTimes() {
+  const times = process.env.TIMES || 5
+  let result = ''
+  for (i = 0; i < times; i++) {
+    result += i + ' ' 
+  }
+  return result
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
